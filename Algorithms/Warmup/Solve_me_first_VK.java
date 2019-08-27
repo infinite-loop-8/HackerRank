8eee1aad76aded2be9b93e5ebb6bb48038d0ff75
infinite-loop-8 <53621113+infinite-loop-8@users.noreply.github.com>
2019-08-28 00:55:26 +0530

    adding codes

---
+++ Algorithms/Warmup/Solve_me_first_VK.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solve_me_first_VK {
+
+
+    
+   
+
+ public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a;
+        a = in.nextInt();
+        int b;
+        b = in.nextInt();
+        int sum;
+        sum = a+b;
+        System.out.println(sum);
+   }
+}
